611aa432ad26aac16857016d3fd3c92c45f5c3b3
romulo.palaoro <romulopalaoro@gmail.com>
2021-01-25 23:03:51 -0300

    código ao final da primeira aula de imersão React

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,51 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from "../db.json";
+import Widget from "../src/components/Widget";
+import Footer from "../src/components/Footer";
+import GitHubCorner from "../src/components/GitHubCorner";
+import QuizBackground from "../src/components/QuizBackground";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+
+
+// const BackgroundImage = styled.div`
+//   flex: 1;
+//   background-position: center;
+//   background-size: cover;
+//   background-image: url(${db.bg});
+// `;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Professora Suellen</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>lorem ipson...</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+            <p>lorem ipson dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer/>
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/romulopalaoro"/>
+    </QuizBackground>
+  )
 }
