ffd359ffe915390398562253b2bdcb2ca308ded6
romulo.palaoro <romulopalaoro@gmail.com>
2021-01-30 18:37:03 -0300

    alteração next/link

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -2,7 +2,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
 import React from 'react';
-import NextLink from 'next/Link';
+import NextLink from 'next/link';
 
 export default function Link({ children, href, ...props }) {
   return (
